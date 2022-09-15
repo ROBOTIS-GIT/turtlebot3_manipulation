fe03ad43388992f750359b19f0f91da95de5bb02
Darby Lim <thlim@robotis.com>
2022-09-15 18:48:21 +0900

    Added launch files
    Signed-off-by: Darby Lim <thlim@robotis.com>

---
+++ turtlebot3_manipulation_bringup/launch/base.launch.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+#
+# Copyright 2012 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Darby Lim
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import Command
+from launch.substitutions import FindExecutable
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    start_rviz = LaunchConfiguration('start_rviz')
+    prefix = LaunchConfiguration("prefix")
+    use_sim = LaunchConfiguration("use_sim")
+    use_fake_hardware = LaunchConfiguration("use_fake_hardware")
+    fake_sensor_commands = LaunchConfiguration("fake_sensor_commands")
+    slowdown = LaunchConfiguration("slowdown")
+
+    urdf_file = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name='xacro')]),
+            ' ',
+            PathJoinSubstitution(
+                [
+                    FindPackageShare('turtlebot3_manipulation_description'),
+                    'urdf',
+                    'turtlebot3_manipulation.urdf.xacro'
+                ]
+            ),
+            ' ',
+            " ",
+            "prefix:=",
+            prefix,
+            " ",
+            "use_sim:=",
+            use_sim,
+            " ",
+            "use_fake_hardware:=",
+            use_fake_hardware,
+            " ",
+            "fake_sensor_commands:=",
+            fake_sensor_commands,
+            " ",
+            "slowdown:=",
+            slowdown,
+        ]
+    )
+
+    controller_manager_config = PathJoinSubstitution(
+        [
+            FindPackageShare("turtlebot3_manipulation_bringup"),
+            "config",
+            "controller_manager.yaml",
+        ]
+    )
+
+    rviz_config_file = PathJoinSubstitution(
+        [
+            FindPackageShare('turtlebot3_manipulation_bringup'),
+            'rviz',
+            'turtlebot3_manipulation.rviz'
+        ]
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'start_rviz',
+            default_value='false',
+            description='Whether execute rviz2'),
+
+        DeclareLaunchArgument(
+            'prefix',
+            default_value='""',
+            description='Prefix of the joint and link names'),
+
+        DeclareLaunchArgument(
+            "use_sim",
+            default_value="false",
+            description="Start robot in Gazebo simulation."),
+
+        DeclareLaunchArgument(
+            "use_fake_hardware",
+            default_value="false",
+            description="Start robot with fake hardware mirroring command to its states."),
+
+        DeclareLaunchArgument(
+            "fake_sensor_commands",
+            default_value="false",
+            description="Enable fake command interfaces for sensors used for simple simulations. \
+            Used only if 'use_fake_hardware' parameter is true."),
+
+        DeclareLaunchArgument(
+            "slowdown",
+            default_value="3.0",
+            description="Slowdown factor."),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': urdf_file}],
+            output='screen'),
+
+        Node(
+            package="controller_manager",
+            executable="ros2_control_node",
+            parameters=[
+                {'robot_description': urdf_file},
+                controller_manager_config
+            ],
+            output={
+                "stdout": "screen",
+                "stderr": "screen",
+            },
+        ),
+
+        Node(
+            package="controller_manager",
+            executable="spawner.py",
+            arguments=["diff_drive_controller"],
+            output="screen",
+        ),
+
+        Node(
+            package="controller_manager",
+            executable="spawner.py",
+            arguments=["joint_state_broadcaster"],
+            output="screen",
+        ),
+
+        Node(
+            package="controller_manager",
+            executable="spawner.py",
+            arguments=["joint_trajectory_position_controller"],
+            output="screen",
+        ),
+
+        Node(
+            package="controller_manager",
+            executable="spawner.py",
+            arguments=["gripper_trajectory_position_controller"],
+            output="screen",
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            arguments=['-d', rviz_config_file],
+            output='screen',
+            condition=IfCondition(start_rviz)),
+    ])
