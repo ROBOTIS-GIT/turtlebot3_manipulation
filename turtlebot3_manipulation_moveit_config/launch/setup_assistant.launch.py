0c5364e9e03914da35f14f000677403c16b89350
Hye-jong KIM <hjkim@robotis.com>
2022-09-16 18:12:03 +0900

    moveit_config pkg
    Signed-off-by: Hye-jong KIM <hjkim@robotis.com>

--- turtlebot3_manipulation_moveit_config/launch/setup_assistant.launch.py
+++ turtlebot3_manipulation_moveit_config/launch/setup_assistant.launch.py
@@ -1,7 +1,30 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_setup_assistant_launch
+#!/usr/bin/env python3
+#
+# Copyright 2020 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors: Hye-jong KIM
+
+######## setup assistant (humble) ########
+#from moveit_configs_utils import MoveItConfigsBuilder
+#from moveit_configs_utils.launches import generate_setup_assistant_launch
 
 
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("turtlebot3_manipulation", package_name="turtlebot3_manipulation_moveit_config").to_moveit_configs()
-    return generate_setup_assistant_launch(moveit_config)
+#def generate_launch_description():
+#    moveit_config = MoveItConfigsBuilder("turtlebot3_manipulation", package_name="turtlebot3_manipulation_moveit_config").to_moveit_configs()
+#    return generate_setup_assistant_launch(moveit_config)
+
+##########################################
+
+# moveit setup assistant is not supported in ros foxy version.
--- turtlebot3_manipulation_moveit_config/launch/warehouse_db.launch.py
+++ turtlebot3_manipulation_moveit_config/launch/warehouse_db.launch.py
@@ -1,7 +1,73 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_warehouse_db_launch
+#!/usr/bin/env python3
+#
+# Copyright 2020 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors: Hye-jong KIM
+
+######## setup assistant (humble) ########
+#from moveit_configs_utils import MoveItConfigsBuilder
+#from moveit_configs_utils.launches import generate_warehouse_db_launch
+
+
+#def generate_launch_description():
+#    moveit_config = MoveItConfigsBuilder("turtlebot3_manipulation", package_name="turtlebot3_manipulation_moveit_config").to_moveit_configs()
+#    return generate_warehouse_db_launch(moveit_config)
+##########################################
+import os
+import yaml
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+import xacro
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("turtlebot3_manipulation", package_name="turtlebot3_manipulation_moveit_config").to_moveit_configs()
-    return generate_warehouse_db_launch(moveit_config)
+
+    """Launch file for warehouse database"""
+    ld = LaunchDescription()
+
+    # The default DB port for moveit (not default MongoDB port to avoid potential conflicts)
+    ld.add_action(DeclareLaunchArgument("moveit_warehouse_port", default_value="33829"))
+
+    # The default DB host for moveit
+    ld.add_action(
+        DeclareLaunchArgument("moveit_warehouse_host", default_value="localhost")
+    )
+
+    # Load warehouse parameters
+    db_parameters = [
+        {
+            "overwrite": False,
+            "warehouse_port": LaunchConfiguration("moveit_warehouse_port"),
+            "warehouse_host": LaunchConfiguration("moveit_warehouse_host"),
+            "warehouse_exec": "mongod",
+            "warehouse_plugin": "warehouse_ros_mongo::MongoDatabaseConnection",
+        },
+    ]
+
+    # Run the DB server
+    db_node = Node(
+        package="warehouse_ros_mongo",
+        executable="mongo_wrapper_ros.py",
+        parameters=db_parameters,
+    )
+    ld.add_action(db_node)
+
+
+    return ld
