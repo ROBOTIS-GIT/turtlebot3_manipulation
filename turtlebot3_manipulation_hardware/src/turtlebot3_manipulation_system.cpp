c0d203d4322f7a20578df9b6723960f31d2270fb
Darby Lim <thlim@robotis.com>
2022-09-20 20:38:45 +0900

    Deleted type casting
    Signed-off-by: Darby Lim <thlim@robotis.com>

--- turtlebot3_manipulation_hardware/src/turtlebot3_manipulation_system.cpp
+++ turtlebot3_manipulation_hardware/src/turtlebot3_manipulation_system.cpp
@@ -139,10 +139,10 @@ TurtleBot3ManipulationSystemHardware::export_command_interfaces()
 hardware_interface::return_type TurtleBot3ManipulationSystemHardware::start()
 {
   RCLCPP_INFO(rclcpp::get_logger("turtlebot3_manipulation"), "Ready for start");
-  dynamixel_sdk_wrapper_->write_byte(opencr_control_table.sound.address, (uint8_t *)1);
+  dynamixel_sdk_wrapper_->write_byte(opencr_control_table.sound.address, 1);
 
-  dynamixel_sdk_wrapper_->write_byte(opencr_control_table.motor_torque.address, (uint8_t *)0);
-  dynamixel_sdk_wrapper_->write_byte(opencr_control_table.torque_joint.address, (uint8_t *)0);
+  dynamixel_sdk_wrapper_->write_byte(opencr_control_table.motor_torque.address, 0);
+  dynamixel_sdk_wrapper_->write_byte(opencr_control_table.torque_joint.address, 0);
 
   status_ = hardware_interface::status::STARTED;
 
