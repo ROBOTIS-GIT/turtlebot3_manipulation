3ce7112b19ab86cde20c504c0ff36700c4ae447c
Hye-jong KIM <hjkim@robotis.com>
2022-10-05 14:50:13 +0900

    remove unuse launchs
    Signed-off-by: Hye-jong KIM <hjkim@robotis.com>

--- turtlebot3_manipulation_moveit_config/launch/move_group.launch.py
+++ turtlebot3_manipulation_moveit_config/launch/move_group.launch.py
@@ -16,15 +16,6 @@
 #
 # Authors: Hye-jong KIM
 
-# setup assistant (humble)
-# from moveit_configs_utils import MoveItConfigsBuilder
-# from moveit_configs_utils.launches import generate_move_group_launch
-# def generate_launch_description():
-#     moveit_config = MoveItConfigsBuilder("turtlebot3_manipulation",
-#                     package_name="turtlebot3_manipulation_moveit_config").to_moveit_configs()
-#     return generate_move_group_launch(moveit_config)
-
-
 import os
 import yaml
 import xacro
